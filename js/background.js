05e1f6176998b64dfc58aa7f8eadd64aefab9ab0
iamvegetable2 <lyz050980@gmail.com>
2022-10-26 17:31:40 +0800

    Site updated: 2022-10-26 17:31:34

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
 var img_array = ["url(/img/1.png)", "url(/img/2.png)", "url(/img/3.png)", "url(/img/4.png)", "url(/img/5.png)", "url(/img/6.png)", "url(/img/7.png)"];
-var index = parseInt(Math.floor(Math.random() * (img_array.length)));
+var index = new Date().getDay();
 var current_background = img_array[index];
 document.getElementById("web_bg").style.backgroundImage = current_background;
